e24f5ef796aaed9defb103bd9725b85cdd7379c2
Bale - Balazs Mocsai <balazs@balazsmocsai.com>
2025-09-05 11:55:07 +0200

    fix: ignore build js files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,22 @@ import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig(
+    { ignores: ['lib/**'] },
     eslint.configs.recommended,
     tseslint.configs.recommended,
     tseslint.configs.stylisticTypeChecked,
     {
         languageOptions: {
             parserOptions: {
-                projectService: true,
+                projectService: {
+                    allowDefaultProject: ['*.mjs'],
+                },
                 tsconfigRootDir: import.meta.dirname,
             },
         },
+    },
+    {
+        files: ['**/*.js'],
+        extends: [tseslint.configs.disableTypeChecked],
     }
 );
